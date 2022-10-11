e721f2fb18a9c423020e36fb1988ff2d8729b876
Michael Bishop <work@michaelbishop.me>
2022-10-11 19:35:46 -0400

    tweak Last.fm recently played

--- .eleventy.js
+++ .eleventy.js
@@ -3,4 +3,15 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
+    eleventyConfig.setBrowserSyncConfig({
+        middleware: [
+            function(req, res, next) {
+                if (/^[^.]+$/.test(req.url)) {
+                    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+                }
+                next();
+            }
+        ]
+    });
 };
