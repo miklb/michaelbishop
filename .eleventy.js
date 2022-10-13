67c3e45b0ffb157784ac5df15e11a9c2ab057be5
Michael Bishop <work@michaelbishop.me>
2022-10-13 12:17:52 -0400

    meta and open graph tags closes Open Graph tags #20

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,12 @@
 const fs = require("fs");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 const { EleventyRenderPlugin } = require("@11ty/eleventy");
+const metagen = require('eleventy-plugin-metagen');
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
     eleventyConfig.addPlugin(EleventyRenderPlugin);
+    eleventyConfig.addPlugin(metagen);
 
     eleventyConfig.setBrowserSyncConfig({
         middleware: [
