d92b3de606b86783c5ea4b47d225b3001562c545
Michael Bishop <work@michaelbishop.me>
2022-10-13 21:16:21 -0400

    header nav
    fixes Navigation #19

--- .eleventy.js
+++ .eleventy.js
@@ -2,11 +2,14 @@ const fs = require("fs");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 const { EleventyRenderPlugin } = require("@11ty/eleventy");
 const metagen = require('eleventy-plugin-metagen');
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
     eleventyConfig.addPlugin(EleventyRenderPlugin);
     eleventyConfig.addPlugin(metagen);
+    eleventyConfig.addPlugin(pluginRss);
 
     eleventyConfig.setBrowserSyncConfig({
         middleware: [
